c096084d180bb4ab409097072deba065d3251b00
Brezhneva13 <165768374+Brezhneva13@users.noreply.github.com>
2024-12-17 18:51:15 +0300

    Update db.php

--- db.php
+++ db.php
@@ -12,7 +12,43 @@ try {
     // Установка режима обработки ошибок
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    echo "Подключение успешно!";
+    echo "Подключение успешно!<br>";
+
+    // Запрос на выборку всех данных из таблицы 'Client'
+    $stmt = $pdo->query("SELECT * FROM Client");
+
+    // Проверка, есть ли данные
+    if ($stmt->rowCount() > 0) {
+        // Вывод заголовков таблицы
+        echo "<table border='1'>
+                <tr>
+                    <th>ClientNumber</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>CardNumber</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Patronymic</th>
+                    <th>BankNumber</th>
+                </tr>";
+
+        // Вывод данных
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>
+                    <td>{$row['ClientNumber']}</td>
+                    <td>{$row['Phone']}</td>
+                    <td>{$row['Address']}</td>
+                    <td>{$row['CardNumber']}</td>
+                    <td>{$row['Name']}</td>
+                    <td>{$row['Surname']}</td>
+                    <td>{$row['Patronymic']}</td>
+                    <td>{$row['BankNumber']}</td>
+                  </tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Нет записей в таблице 'Client'.";
+    }
 } catch (PDOException $e) {
     // Обработка ошибок подключения
     echo "Ошибка подключения: " . $e->getMessage();
